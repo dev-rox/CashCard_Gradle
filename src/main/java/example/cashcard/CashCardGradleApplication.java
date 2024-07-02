8faa2cc4e6b38ae9c0c85cf4df84c5d36cb9fa89
rox-dev <142692001+dev-rox@users.noreply.github.com>
2024-07-02 05:51:55 -0300

    Initial commit

---
+++ src/main/java/example/cashcard/CashCardGradleApplication.java
@@ -0,0 +1,13 @@
+package example.cashcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashCardGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashCardGradleApplication.class, args);
+    }
+
+}
